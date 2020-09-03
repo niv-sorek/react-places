f934148426f623416d34448ba274652e249394d2
Niv Sorek <39373160+niv-sorek@users.noreply.github.com>
2020-09-03 11:54:44 +0300

    starter

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <h1 title="this is working">Hello, Niv</h1>
-  );
+  return <div>
+    <h2>Hello, world!</h2>
+    <ul className="my-list">
+      <li>Niv</li>
+      <li>Sorek</li>
+      <li>is here</li>
+    </ul>
+  </div>
 }
 
 export default App;
